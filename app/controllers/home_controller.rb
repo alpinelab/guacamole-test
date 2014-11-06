021ca861c1e21739182654335854ba31a4bc8b32
thibaultdalban <thibault.dalban@alpine-lab.com>
2014-11-06 23:03:43 +0100

    Raise up tring size to 10M

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    a_string = "a" * 1.megabyte
-    b_string = "b" * 1.megabyte
+    a_string = "a" * 10.megabyte
+    b_string = "b" * 10.megabyte
     if a_string == b_string.gsub(/b/, "a")
       @var = a_string + b_string
     end
