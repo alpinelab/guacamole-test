e0db7d207d33248649804a0506e4194e76ed9148
Michael Baudino <michael.baudino@alpine-lab.com>
2014-11-07 03:34:05 +0100

    Remove memory eating lines

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,6 @@ class HomeController < ApplicationController
     (1024*ENV['MEGABYTES_OF_OUTPUT_PER_REQUEST'].to_i).times do
       puts 'a' * 1024
     end
-    a_string = "a" * 10.megabyte
-    b_string = "b" * 10.megabyte
-    if a_string == b_string.gsub(/b/, "a")
-      @var = a_string + b_string
-    end
+    render text: 'OK'
   end
 end
