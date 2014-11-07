67449a1f53c51d6ac4e20b7e66e4024246de4cfd
Michael Baudino <michael.baudino@alpine-lab.com>
2014-11-07 02:49:10 +0100

    Change output method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    puts 'a' * ENV['MEGABYTES_OF_OUTPUT_PER_REQUEST'].to_i.megabytes
+    (1024*ENV['MEGABYTES_OF_OUTPUT_PER_REQUEST'].to_i).times do
+      puts 'a' * 1024
+    end
     a_string = "a" * 10.megabyte
     b_string = "b" * 10.megabyte
     if a_string == b_string.gsub(/b/, "a")
